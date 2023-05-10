072b40e2fa05d8b535abf6fdb674ca3d2677641c
Markus Heinilä <markus.heinila@pp.inet.fi>
2023-05-10 14:30:16 +0300

    form adds a recipe to db.json now

--- src/pages/Adding.js
+++ src/pages/Adding.js
@@ -2,8 +2,27 @@ import React from 'react';
 import axios from 'axios';
 
 const Adding = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const recipe = {
+          name: form.name.value,
+          author: form.author.value,
+          description: form.description.value,
+          image: form.image.value,
+          instructions: form.instructions.value,
+        };
+        axios.post('http://localhost:4002/recipes', recipe)
+          .then(response => {
+            console.log(response.data);
+            form.reset();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      };
     return (
-        <form className="form" action='submit'>
+        <form className="form" onSubmit={handleSubmit}>
             <label htmlFor ="name">Name</label>
             <input type="text" name="name"></input>
             <label htmlFor ="author">Author</label>
