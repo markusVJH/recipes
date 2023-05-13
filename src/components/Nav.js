477ef02a817f5851fc28bab4acb255a7d132056a
Markus Heinilä <markus.heinila@pp.inet.fi>
2023-05-13 14:59:57 +0300

    hero banner video added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,9 +7,9 @@ const Nav = () => {
             <p className='logo'><NavLink to="/">Super Recipes!</NavLink></p>
         <nav className="nav">
             <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/recipes">Recipes</NavLink></li>
-                <li><NavLink to="/adding">Add new recipe</NavLink></li>
+                <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/recipes" activeClassName="active">Recipes</NavLink></li>
+                <li><NavLink to="/adding" activeClassName="active">Add new recipe</NavLink></li>
             </ul>
         </nav>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import video from '../media/video.mp4'
 
 const Home = () => {
     return (
         <div className='fullpage'>
             <div className='heroBanner'>
-                <h1>Super Recipes</h1>
-                <p>Welcome to Super Recipes!! :) This is a Business College Helsinki project</p>
-                <Link to={`/recipes`}>Browse recipes</Link>
+                <video autoPlay loop muted>
+                    <source src={video} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <div className="overlay">
+                    <h1>Super Recipes</h1>
+                    <p>Welcome to Super Recipes!! :) This is a Business College Helsinki project</p>
+                    <Link to={`/recipes`}>Browse recipes</Link>
+                </div>
             </div>
             <div className='homelinks'>
             <div className='homebox'>
