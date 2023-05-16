a4c6e7f20696f49b6d4fbab73338ad57ba820e48
Markus Heinilä <markus.heinila@pp.inet.fi>
2023-05-16 18:41:20 +0300

    speed game box for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,14 +18,19 @@ const Home = () => {
             <div className='homelinks'>
               <div className='homebox'>
                 <h3>Recipes</h3>
-                <p>Check out all the recipes here. You can search them by recipe name.</p>
+                <p>Check out all the recipes here. You can search them by name.</p>
                 <Link to={`/recipes`}>All recipes</Link>
               </div>
-                <div className='homebox'>
-                  <h3>Add new recipe</h3>
-                  <p>Want to add a new recipe? You can do so here :)</p>
-                  <Link to={`/adding`}>Add a new recipe</Link>
-                </div>
+              <div className='homebox'>
+                <h3>Add new recipe</h3>
+                <p>Want to add a new recipe? You can do so here :)</p>
+                <Link to={`/adding`}>Add a new recipe</Link>
+              </div>
+              <div className='homebox'>
+                <h3>Bored while food is getting ready?</h3>
+                <p>Try our speed game to pass the time :)</p>
+                <a target="blank" href='https://markusvjh.github.io/React_speed_game/'>Speed Game</a>
+              </div>
               <div className='homebox'>
                 <h3>Want to know more about our projects?</h3>
                 <p>Visit our school's <a target="blank" href='https://en.bc.fi/'>homepage</a></p>
